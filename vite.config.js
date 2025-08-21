c76aa0dc74b0de93c0d3e055b6da90c4bb703d2a
A Damian Garcia <adamiangarcia93@gmail.com>
2025-08-21 19:53:48 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// REEMPLAZÁ 'inventario-henko-web' por el nombre de tu repositorio
 export default defineConfig({
   plugins: [react()],
-  base: '/inventario-henko-web/',
+  base: '/inventario-RIO-web/',
 })
