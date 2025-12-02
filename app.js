1af9ce5ddcb3ee243fb3a1ab8d113933d34c0229
DamyxDev <adamiangarcia93@gmail.com>
2025-12-02 16:28:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
   // ====== Config ======
   const RESPONSABLES = ["DAVID","DIEGO","JOEL","MARTIN","MIGUEL","NAHUEL","RICARDO","RODRIGO"];
-  const SUCURSALES  = ["AV2","NAZCA","LAMARCA","CORRIENTES","CO2","CASTELLI","QUILMES","MORENO","SARMIENTO","DEPOSITO"];
+  const SUCURSALES  = ["AV2","NAZCA","LAMARCA","CORRIENTES","CO2","CASTELLI","QUILMES","MORENO","SARMIENTO","DEPOSITO","PUEYRREDON"];
   const CSV_FILES   = ["equivalencia.csv", "equivalencia2.csv"]; // ambos si existen
   const LS_META  = "pickeo_meta_v1";
   const AUTOCOMMIT_IDLE_MS = 80;
